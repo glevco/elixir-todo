945e9c7d721d49e7fbae53d9b07c42282c30a0f2
Gabriel Levcovitz <g.levcovitz@gmail.com>
2020-05-17 20:35:21 -0300

    Refactor database

--- lib/todo/database.ex
+++ lib/todo/database.ex
@@ -9,47 +9,38 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
   end
 
   def init(_) do
     File.mkdir_p!(@db_folder)
-    worker_nums = 0..(@num_workers - 1)
-    worker_generator = &{&1, get_worker()}
-    workers = Map.new(worker_nums, worker_generator)
 
-    {:ok, workers}
+    {:ok, start_workers()}
   end
 
-  def handle_cast({:store, key, data}, workers) do
-    workers
-    |> choose_worker(key)
-    |> Todo.DatabaseWorker.store(key, data)
-
-    {:noreply, workers}
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
   end
 
-  def handle_call({:get, key}, _, workers) do
-    data =
-      workers
-      |> choose_worker(key)
-      |> Todo.DatabaseWorker.get(key)
-
-    {:reply, data, workers}
-  end
-
-  defp get_worker do
-    {_, worker} = Todo.DatabaseWorker.start(@db_folder)
-
-    worker
-  end
-
-  def choose_worker(workers, key) do
+  def handle_call({:choose_worker, key}, _, workers) do
     hash = :erlang.phash2(key, @num_workers)
-    workers[hash]
+
+    {:reply, workers[hash], workers}
+  end
+
+  defp start_workers do
+    for index <- 1..@num_workers, into: %{} do
+      {:ok, worker} = Todo.DatabaseWorker.start(@db_folder)
+
+      {index - 1, worker}
+    end
   end
 end
